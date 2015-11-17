70d5e59b356bc1948dcae170eafa347e6a84a3f8
alest <alest@Alejandro>
2015-11-17 11:05:14 -0600

    OK

--- Biblioteca/src/main/java/com/itgam/power/rangers/control/InicioSistemaController.java
+++ Biblioteca/src/main/java/com/itgam/power/rangers/control/InicioSistemaController.java
@@ -6,10 +6,12 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class InicioSistemaController {
 
 private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -17,7 +19,7 @@ private static final Logger logger = LoggerFactory.getLogger(HomeController.clas
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/ITGAM", method = RequestMethod.GET)
+	@RequestMapping(value = "/itgam", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
--- Biblioteca/src/main/java/com/itgam/power/rangers/control/ct/RegistroController.java
+++ Biblioteca/src/main/java/com/itgam/power/rangers/control/ct/RegistroController.java
@@ -1,14 +1,26 @@
 package com.itgam.power.rangers.control.ct;
 
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.itgam.power.rangers.model.ctUsuario;
+import com.itgam.power.rangers.service.ct.UsuarioService;
+
+@Controller
 public class RegistroController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
 
 	@RequestMapping(value = "/itgam/registro", method = RequestMethod.GET)
 	public String registro(Locale locale, Model model) {
@@ -24,8 +36,27 @@ public class RegistroController {
 	}
 	
 	@RequestMapping(value = "/itgam/registro/usuario", method = RequestMethod.POST)
-	public String home(Locale locale, Model model) {
+	public String home(@ModelAttribute("ctUsuario") ctUsuario obj_ctUsuario) throws SQLException {
 		
+		/*Prueba*/
+		
+		obj_ctUsuario.setcNombre("Alex");
+		obj_ctUsuario.setcApellidos("Estrada");
+		obj_ctUsuario.setcCalle("AS");
+		obj_ctUsuario.setcNumExterior("67");
+		obj_ctUsuario.setcNumInterior("2");
+		obj_ctUsuario.setcColonia("Vallejo");
+		obj_ctUsuario.setcCP("07870");
+		obj_ctUsuario.setcEstado("DF");
+		obj_ctUsuario.setcEmail("alestra55@hotmail.com");
+		//obj_ctUsuario.setdtFechaNac("2015/11/17 00:00:00.000000");
+		obj_ctUsuario.setlSexo(true);
+		obj_ctUsuario.setcUsuario("alex");
+		obj_ctUsuario.setcPassword("123");
+		obj_ctUsuario.setlEstatus(true);
+		obj_ctUsuario.setcObs("XD");
+		/*Elimar en cuanto deje de ser funcional*/
+		usuarioService.addctUsuario(obj_ctUsuario);
 		return "inicioSistema";
 	}
 }
