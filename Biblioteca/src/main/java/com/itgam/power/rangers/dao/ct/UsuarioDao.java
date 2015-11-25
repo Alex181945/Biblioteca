1cfe05728215cb09c27b7581715960af0bf571b7
alest <alest@Alejandro>
2015-11-25 11:09:38 -0600

    OK

--- Biblioteca/src/main/java/com/itgam/power/rangers/dao/ct/UsuarioDao.java
+++ Biblioteca/src/main/java/com/itgam/power/rangers/dao/ct/UsuarioDao.java
@@ -1,6 +1,7 @@
 package com.itgam.power.rangers.dao.ct;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import com.itgam.power.rangers.model.ctUsuario;
 
@@ -13,4 +14,8 @@ public interface UsuarioDao {
 	public void deletectUsuario (Integer id_ctUsuario);
 
 	public Boolean loginctUsuario (ctUsuario obj_ctUsuario) throws SQLException;
+	
+	public List<ctUsuario> list_ctUsuario();
+	
+	public ctUsuario get_ctUsuario(Integer id_ctUsuario);
 }
--- Biblioteca/src/main/java/com/itgam/power/rangers/dao/impl/ct/UsuarioDaoImpl.java
+++ Biblioteca/src/main/java/com/itgam/power/rangers/dao/impl/ct/UsuarioDaoImpl.java
@@ -2,6 +2,8 @@ package com.itgam.power.rangers.dao.impl.ct;
 
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
@@ -62,12 +64,74 @@ public class UsuarioDaoImpl implements UsuarioDao {
 	}
 	
 	public void updatectUsuario (ctUsuario obj_ctUsuario){
-            
+		
+		/*Conexion a la base de datos*/
+		DBConexion conexion = new DBConexion();
+		Connection inicio = conexion.GetConnection();
+
+		try{
+			/*Creacion de la variable que almacenara la sentencia SQL*/
+			Statement consulta = inicio.createStatement();
+			
+			/*Sentencia SQL*/
+			String sentencia = null;
+			
+			sentencia = "UPDATE ctUsuario SET cNombre = \""+obj_ctUsuario.getcNombre()+"\","
+					+ "cApellidos = \" "+obj_ctUsuario.getcApellidos()+",cCalle = \""+obj_ctUsuario.getcCalle()+"\","
+							+ "cNumExterior = \""+obj_ctUsuario.getcNumExterior()+",cNumInterior = \""+obj_ctUsuario.getcNumInterior()+","
+							   + "cColonia = \""+obj_ctUsuario.getcColonia() + ", cCP = \" "+obj_ctUsuario.getcCP()
+							     + ", cEstado = \""+obj_ctUsuario.getcEstado()+ "," + ", cEmail = \""+obj_ctUsuario.getcEmail()
+							       + obj_ctUsuario.getDtFechaNac()+ "," + ""+obj_ctUsuario.getlSexo()+
+							       "ctUsuario = \""+obj_ctUsuario.getcUsuario()+ "," +  "cPassword = \""+obj_ctUsuario.getcPassword()+
+							            "lEstatus = "+obj_ctUsuario.getlEstatus()+ "cObs = \""+obj_ctUsuario.getcObs()+"\" WHERE iUsuario = "+obj_ctUsuario.getiUsuario()+"";
+			System.out.println(sentencia);
+			consulta.executeUpdate(sentencia);
+			
+		}catch(Exception e){
+			System.out.println("Algo salio mal");
+		}
+		finally{
+			/*Terminando conexion con la base de datos*/
+			System.out.println("Termina conexion");
+			try {
+				inicio.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
             
 	}
 	
 	public void deletectUsuario (Integer id_ctUsuario){
 		
+		/*Conexion a la base de datos*/
+		DBConexion conexion = new DBConexion();
+		Connection inicio = conexion.GetConnection();
+		
+		try{
+			/*Creacion de la variable que almacenara la sentencia SQL*/
+			Statement consulta = inicio.createStatement();
+			
+			/*Sentencia SQL*/
+			String sentencia = null;
+			
+			sentencia = "DELETE FROM ctUsuario WHERE ctUsuario = "+id_ctUsuario+"";
+			consulta.execute(sentencia);
+			
+		}catch (Exception e){
+			System.out.println("Hubo un problema: "+e);
+		}
+		finally{
+			/*Terminando conexion con la base de datos*/
+			System.out.println("Termina conexion");
+			try {
+				inicio.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public Boolean loginctUsuario (ctUsuario obj_ctUsuario) throws SQLException{
@@ -92,5 +156,121 @@ public class UsuarioDaoImpl implements UsuarioDao {
 		}
 		System.out.println(respuesta);
 		return respuesta;
+	}
+	
+	public List<ctUsuario> list_ctUsuario(){
+		
+		/*Creacion de la lista que almacenara los datos*/
+		List<ctUsuario> Lista = new ArrayList<ctUsuario>();
+		
+		/*Conexion a la base de datos*/
+		DBConexion conexion = new DBConexion();
+		Connection inicio = conexion.GetConnection();
+		
+		try{
+			/*Creacion de la variable que almacenara la sentencia SQL*/
+			Statement consulta = inicio.createStatement();
+			
+			/*Sentencia SQL*/
+			String sentencia = null;
+			
+			sentencia = "SELECT * FROM ctUsuario";
+			ResultSet res = consulta.executeQuery(sentencia);
+			
+			while(res.next()){
+				ctUsuario obj = new ctUsuario();
+				
+				obj.setiUsuario (res.getInt("iUsuario"));
+				obj.setcNombre(res.getString("cNombre"));
+				obj.setcApellidos(res.getString("cApellidos"));
+				obj.setcCalle(res.getString("cCalle"));
+				obj.setcNumExterior(res.getString("cNumExterior"));
+				obj.setcNumInterior(res.getString("cNumInterior"));
+				obj.setcColonia(res.getString("cColonia"));
+				obj.setcCP (res.getString("cCP "));
+				obj.setcEstado(res.getString("cEstado"));
+				obj.setcEmail(res.getString("cEmail"));
+				obj.setDtFechaNac(res.getString("dtFechaNac"));
+				obj.setcEmail(res.getString("cEmail"));
+				obj.setlSexo(res.getBoolean("lSexo"));
+				obj.setcUsuario(res.getString("cUsuario"));
+				obj.setcPassword(res.getString("cPassword"));
+				obj.setlSexo(res.getBoolean("lSexo"));
+				obj.setcObs(res.getString("cObs"));
+				
+				Lista.add(obj);
+			}
+			
+		}catch (Exception e){
+			System.out.println("Hubo un problema: "+e);
+		}
+		finally{
+			/*Terminando conexion con la base de datos*/
+			System.out.println("Termina conexion");
+			try {
+				inicio.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return Lista ;
+	}
+	
+	public ctUsuario get_ctUsuario(Integer id_ctUsuario){
+		
+		/*Creacion de la lista que almacenara los datos*/
+		ctUsuario obj = new ctUsuario();
+		
+		/*Conexion a la base de datos*/
+		DBConexion conexion = new DBConexion();
+		Connection inicio = conexion.GetConnection();
+		
+		try{
+			/*Creacion de la variable que almacenara la sentencia SQL*/
+			Statement consulta = inicio.createStatement();
+			
+			/*Sentencia SQL*/
+			String sentencia = null;
+			
+			sentencia = "SELECT * FROM ctUsuarii WHERE cUsuario = "+id_ctUsuario+"";
+			ResultSet res = consulta.executeQuery(sentencia);
+			
+			while(res.next()){
+				
+				obj.setiUsuario (res.getInt("iUsuario"));
+				obj.setcNombre(res.getString("cNombre"));
+				obj.setcApellidos(res.getString("cApellidos"));
+				obj.setcCalle(res.getString("cCalle"));
+				obj.setcNumExterior(res.getString("cNumExterior"));
+				obj.setcNumInterior(res.getString("cNumInterior"));
+				obj.setcColonia(res.getString("cColonia"));
+				obj.setcCP (res.getString("cCP "));
+				obj.setcEstado(res.getString("cEstado"));
+				obj.setcEmail(res.getString("cEmail"));
+				obj.setDtFechaNac(res.getString("dtFechaNac"));
+				obj.setcEmail(res.getString("cEmail"));
+				obj.setlSexo(res.getBoolean("lSexo"));
+				obj.setcUsuario(res.getString("cUsuario"));
+				obj.setcPassword(res.getString("cPassword"));
+				obj.setlSexo(res.getBoolean("lSexo"));
+				obj.setcObs(res.getString("cObs"));
+			}
+			
+		}catch (Exception e){
+			System.out.println("Hubo un problema: "+e);
+		}
+		finally{
+			/*Terminando conexion con la base de datos*/
+			System.out.println("Termina conexion");
+			try {
+				inicio.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return obj;
 	}
 }
