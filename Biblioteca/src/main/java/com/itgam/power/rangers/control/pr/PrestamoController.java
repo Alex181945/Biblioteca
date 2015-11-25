4b7defd9f1337292587ed23b8524896a90b60810
alest <alest@Alejandro>
2015-11-25 00:28:06 -0600

    OK

--- Biblioteca/src/main/java/com/itgam/power/rangers/control/pr/PrestamoController.java
+++ Biblioteca/src/main/java/com/itgam/power/rangers/control/pr/PrestamoController.java
@@ -1,13 +1,13 @@
 package com.itgam.power.rangers.control.pr;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class PrestamoController {
 	
 	@RequestMapping(value = "/prestamo", method = RequestMethod.GET)
